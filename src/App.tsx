ee5d714ba992807e6df3f5417bf9f6a1302999f5
M3n0nJ0rn3y <osk.michalski@proton.me>
2025-09-12 14:40:18 +0200

    Added a navigation bar and one additional page (Education). Next step is to create the rest of pages in nav bar, and add the dropdown-hover function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Get_Education from "./Education";
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,7 +7,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/Education">Education</Link>
+      </nav>
       <Routes>
+        <Route path="/Education" element={<Get_Education />} />
         <Route path="/" element={
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
---
+++ src/Education.tsx
@@ -0,0 +1,10 @@
+function Get_Education() {
+    return (
+        <div>
+            <h1> This is my page with my Education! </h1>
+            <p> I have a lot of education! YEs </p>
+        </div>
+    );
+}
+
+export default Get_Education;
