16f47932e78d5b756e40b7d85752524cd576130c
M3n0nJ0rn3y <osk.michalski@proton.me>
2025-09-12 14:07:41 +0200

    Initial setup of my WebApp, added routers as preparation to developing a navigation bar.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
